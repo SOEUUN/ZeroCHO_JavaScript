905a73e03f89ff6419e92335ef55c2c55123d9bf
thdmsdl97 <thdmsdl97@gmail.com>
2022-11-14 23:31:12 +0900

    Calculator

---
+++ class/calculator.js
@@ -0,0 +1,117 @@
+  // let numOne = '';
+  // let operator = '';
+  // let numTwo = '';
+
+  // const $operater = document.querySelector('#operator');  //태그 선택(변수 저장할때)
+  // const $result = document.querySelector('#result');      //태그 선택(변수 저장할때)
+  // const onClickNumber = (event) => { 
+  //     if(operator){         // 연산자를 눌렀다면
+  //       numTwo += event.target.textContent;      // 두번째 숫자에 0을 입력
+  //     }else{
+  //       numOne += event.target.textContent;      // 누르지 않았다면 첫번째 숫자에 0 입력
+  //     }
+  //     $result.value += event.target.textContent; // 화면에 출력
+    
+
+
+  //   // return () => {
+  //   //   if(operator){         // 연산자를 눌렀다면
+  //   //     numTwo += number;      // 두번째 숫자에 0을 입력
+  //   //   }else{
+  //   //     numOne += number;      // 누르지 않았다면 첫번째 숫자에 0 입력
+  //   //   }
+  //   //   $result.value += number; // 화면에 출력
+  //   // };
+
+  //   // return undefined; 함수는 (리턴이 없으면) undefined를 리턴함 
+
+  //   // return () => {
+  //   //   // 안의 내용이 비어 있기 때문에 내용을 채워 넣어 주어야함
+  //   // };
+
+  //   // 화살표 함수는 중괄호와 return이 있으면 생략 가능함
+  // }; 
+
+  // // 함수에서 중복이 발생 했을 경우 (중복 되는 부분을 찾아서 매개변수로 빼기)
+
+  // document.querySelector('#num-0').addEventListener('click', onClickNumber); //('click', 함수자리(onClickNumber의 return자리))
+  // document.querySelector('#num-1').addEventListener('click', onClickNumber); // 1을 누르면 1의 textContent는 1이 됨 (textContent는 문자열)
+  // document.querySelector('#num-2').addEventListener('click', onClickNumber);
+  // document.querySelector('#num-3').addEventListener('click', onClickNumber);
+  // document.querySelector('#num-4').addEventListener('click', onClickNumber);
+  // document.querySelector('#num-5').addEventListener('click', onClickNumber);
+  // document.querySelector('#num-6').addEventListener('click', onClickNumber);
+  // document.querySelector('#num-7').addEventListener('click', onClickNumber);
+  // document.querySelector('#num-8').addEventListener('click', onClickNumber);
+  // document.querySelector('#num-9').addEventListener('click', onClickNumber);
+  // document.querySelector('#plus').addEventListener('click', () => {});
+  // document.querySelector('#minus').addEventListener('click', () => {});
+  // document.querySelector('#divide').addEventListener('click', () => {});
+  // document.querySelector('#multiply').addEventListener('click', () => {});
+  // document.querySelector('#calculate').addEventListener('click', () => {});
+  // document.querySelector('#clear').addEventListener('click', () => {});
+
+  let numOne = '';
+  let operator = '';
+  let numTwo = '';
+
+  const $operater = document.querySelector('#operator');  //태그 선택(변수 저장할때)
+  const $result = document.querySelector('#result');      //태그 선택(변수 저장할때)
+  const onClickNumber = (number) => (event) => {
+      if(operator){         // 연산자를 눌렀다면
+        numTwo += number;      // 두번째 숫자에 0을 입력
+      }else{
+        numOne += number;      // 누르지 않았다면 첫번째 숫자에 0 입력
+      }
+      $result.value += number; // 화면에 출력
+    
+
+
+    // return () => {
+    //   if(operator){         // 연산자를 눌렀다면
+    //     numTwo += number;      // 두번째 숫자에 0을 입력
+    //   }else{
+    //     numOne += number;      // 누르지 않았다면 첫번째 숫자에 0 입력
+    //   }
+    //   $result.value += number; // 화면에 출력
+    // };
+
+    // return undefined; 함수는 (리턴이 없으면) undefined를 리턴함 
+
+    // return () => {
+    //   // 안의 내용이 비어 있기 때문에 내용을 채워 넣어 주어야함
+    // };
+
+    // 화살표 함수는 중괄호와 return이 있으면 생략 가능함
+  };  // 고차 함수(high order function / 함수가 함수를 리턴 하는 것)
+
+  // 함수에서 중복이 발생 했을 경우 (중복 되는 부분을 찾아서 매개변수로 빼기)
+
+  document.querySelector('#num-0').addEventListener('click', onClickNumber('0')); //('click', 함수자리(onClickNumber의 return자리))
+  document.querySelector('#num-1').addEventListener('click', onClickNumber('1'));
+  document.querySelector('#num-2').addEventListener('click', onClickNumber('2'));
+  document.querySelector('#num-3').addEventListener('click', onClickNumber('3'));
+  document.querySelector('#num-4').addEventListener('click', onClickNumber('4'));
+  document.querySelector('#num-5').addEventListener('click', onClickNumber('5'));
+  document.querySelector('#num-6').addEventListener('click', onClickNumber('6'));
+  document.querySelector('#num-7').addEventListener('click', onClickNumber('7'));
+  document.querySelector('#num-8').addEventListener('click', onClickNumber('8'));
+  document.querySelector('#num-9').addEventListener('click', onClickNumber('9'));
+
+  const onClickOperator = (op) => () => {
+      if(numOne){       // 첫번째 숫자가 입력 되었다면
+      operator = op;  // 연산자 입력
+      $operater.value = op;
+      }else{
+        alert('숫자를 먼저 입력하세요.');
+      }
+    
+  };
+
+  document.querySelector('#plus').addEventListener('click', onClickOperator('+'));
+  document.querySelector('#minus').addEventListener('click', onClickOperator('-'));
+  document.querySelector('#divide').addEventListener('click', onClickOperator('/'));
+  document.querySelector('#multiply').addEventListener('click', onClickOperator('*'));
+  document.querySelector('#calculate').addEventListener('click', () => {});
+  document.querySelector('#clear').addEventListener('click', () => {});
+  
